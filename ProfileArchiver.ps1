31884d105fd33a3064d58d7d1478abe1cf14cbd6
bkeegan <brenton.keegan@gmail.com>
2016-04-02 16:59:56 -0400

    Update ProfileArchiver.ps1

--- ProfileArchiver.ps1
+++ ProfileArchiver.ps1
@@ -15,6 +15,8 @@
 .EXAMPLE 
 
 #> 
+
+#Imports
 import-module activedirectory
 
 Function ProfileArchiver 
@@ -45,34 +47,20 @@ Function ProfileArchiver
 		
 		[parameter(Mandatory=$false)]
 		[alias("Subject")]
-		[string]$emailSubject="AutoArchive Report"
+		[string]$emailSubject="AutoArchive Report",
+		
+		[parameter(Mandatory=$false)]
+		[alias("body")]
+		[string]$emailBody="Archive Report - See Attachment"
 	)
 
-	#$foldersToCheck = "\\limcollege.edu\root\profiles\undergrad"
-	#$archiveLocation = "\\limcollege.edu\root\profiles\graduated\u"
-
-	#stores profile information for profiles with no matching AD user and no abnormalities in the ACL
-	$usersNotExist = @()
-
-	#stores profile information for profiles with abnormal ACL. will be reported on for manual intervention
-	$foldersWithAbnormalACL = @()
-
-	#stores profile paths of profiles that were unable to archive successfully - will be reported on for manual intervention
-	$errorArchiving =@()
-
-	#stores profile paths of profiles that archived successfully but could not be deleted - will be reported on for manual intervention
-	$errorDeleting =@()
-
-	#stores profile paths that were successfully archived and originals were deleted. 
-	$successArchiveDelete = @()
-
-
+	$archiveResults = new-object 'system.collections.generic.dictionary[string,string]'	
 	$profileFolders = get-childitem -Directory -path $foldersToCheck
 
 	#datestamp - a folder will be created under the archive location where old profiles will be archived to. This is to prevent issues and/or data being overwritten from consectuive archive attempts
 	#each archive attempt should be a new file location
 	[string]$dateStamp = Get-Date -UFormat "%Y%m%d_%H%M%S"
-
+	$tempFolder = get-item env:temp
 
 	foreach($folder in $profileFolders)
 	{
@@ -102,14 +90,8 @@ Function ProfileArchiver
 					#expected ACL entries to ignore - if additional ACL entries are identified as expected, add to here
 					"CREATOR OWNER Allow  FullControl" {}
 					"NT AUTHORITY\\SYSTEM Allow  FullControl" {}
-					"LIM\\User Profile IT Access Allow  FullControl" {}
 					"BUILTIN\\Administrators Allow  FullControl" {}
-					"LIM\\bkadmin Allow  FullControl" {}
 					"LIM\\Domain Admins Allow  FullControl" {}
-					"CREATOR OWNER Allow  268435456" {}
-					"BUILTIN\\Users Allow  CreateFiles" {}
-					"BUILTIN\\Users Allow  ReadAndExecute, Synchronize" {}
-					"BUILTIN\\Users Allow  AppendData" {}
 					#regex to detect broken SID
 					"S-1-5.+" {
 						
@@ -129,6 +111,8 @@ Function ProfileArchiver
 			if($abnormalEntries -eq $true)
 			{
 				$foldersWithAbnormalACL += $folder.FullName
+				$archiveResults.Add($folder.FullName,"Abnormal ACL")
+
 			}
 			
 			if(($brokenSID -eq $true) -and ($abnormalEntries -eq $false))
@@ -150,7 +134,7 @@ Function ProfileArchiver
 				Catch
 				{
 					$archError = $true
-					$errorArchiving += $Folder.FullName
+					$archiveResults.Add($folder.FullName,"ArchiveError")
 				}
 
 				if(!($archError))
@@ -158,50 +142,26 @@ Function ProfileArchiver
 					$deleteError = $false
 					Try
 					{
-						#Remove-Item $folder.Fullname -Force -Recurse
+						Remove-Item $folder.Fullname -Force -Recurse
 					}
 					Catch
 					{
-						$errorDeleting += $folder.FullName
+						$archiveResults.Add($folder.FullName,"DeleteError")
 						$deleteError = $true
 					}
 					
 					if(!($deleteError))
 					{
-						$successArchiveDelete += $folder.FullName
+						$archiveResults.Add($folder.FullName,"Success")
 					}
 				}	
 			}
 		}
 	}
-
-	#special builtin variable "Output Field Seperator" 
-	#default is a single whitespace, change below makes it a new line. This makes it so when the arrays are put in the here-string each entry is put on a new line.
-	$ofs = "`r`n"
-
-#string cannot be indented because tab will be interpreted in the string output.
-$emailBody = "
-=======================================ABNORMAL ACL====================================
-THESE PATHS LEAST ONE ACL ENTRY THAT IS NOT A SYSTEM ACCOUNT OR A BROKEN SID. 
-TYPICALLY THIS IS ANOTHER USER ACCOUNT. MANUAL INTERVENTION IS REQUIRED.
-
-$foldersWithAbnormalACL
-================================FOLDERS UNABLE TO ARCHIVE==============================
-THE ZIP OPERATION FAILED ON THE FOLLOWING PATHS. 
-ATTEMPT MANUAL ZIP AND COPY TO ARCHIVE LOCATION. DELETE ORIGINAL PATH ONCE COMPLETED 
-
-$errorArchiving
-===================================UNABLE TO DELETE======================================
-PATHS BELOW WERE SUCCESSFULLY ARCHIVED BUT COULD NOT BE DELETED. 
-ENTERIES HERE SHOULD BE DELETED MANUALLY.
-
-$errorDeleting
-====================FOLDERS SUCCESSFULLY ARCHIVED AND DELETED==========================
-PATHS BELOW WERE SUCCESSFULLY ARCHIVED AND ORIGINALS WERE DELETED.
-NO ADDITIONAL ACTION REQUIRED.
-
-$successArchiveDelete"
 	
+	$archiveResults.GetEnumerator() | Sort-Object -property Value | ConvertTo-HTML | Out-File "$($tempFolder.value)\$dateStamp-Report.html"
+
 	
-	Send-MailMessage -To $emailRecipient -Subject $emailSubject -smtpServer $emailServer -From $emailSender -body $emailBody
+	Send-MailMessage -To $emailRecipient -Subject $emailSubject -smtpServer $emailServer -From $emailSender -body $emailBody -Attachments "$($tempFolder.value)\$dateStamp-Report.html"
+
 }
